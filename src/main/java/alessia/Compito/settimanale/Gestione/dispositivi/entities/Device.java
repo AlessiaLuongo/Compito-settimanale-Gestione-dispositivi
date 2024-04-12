0384001fe0e1d3f5d3e7967b3d5f346343d98fd6
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-04-12 10:34:48 +0200

    creazione entities okey

---
+++ src/main/java/alessia/Compito/settimanale/Gestione/dispositivi/entities/Device.java
@@ -0,0 +1,31 @@
+package alessia.Compito.settimanale.Gestione.dispositivi.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+@Entity
+@Table(name = "devices")
+public class Device {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private int id;
+    private String type;
+    private String status;
+
+    @ManyToOne
+    private Employee employee;
+
+
+    public Device(String type, String status) {
+        this.type = type;
+        this.status = status;
+    }
+}
