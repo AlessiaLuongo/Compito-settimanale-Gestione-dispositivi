7d2cc4686c78380fc6d6ff1a4ff12cfdb5ed5c95
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-04-12 10:05:04 +0200

    initial commit

---
+++ src/main/java/alessia/Compito/settimanale/Gestione/dispositivi/CompitoSettimanaleGestioneDispositiviApplication.java
@@ -0,0 +1,13 @@
+package alessia.Compito.settimanale.Gestione.dispositivi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompitoSettimanaleGestioneDispositiviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompitoSettimanaleGestioneDispositiviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alessia/Compito/settimanale/Gestione/dispositivi/CompitoSettimanaleGestioneDispositiviApplicationTests.java
@@ -0,0 +1,13 @@
+package alessia.Compito.settimanale.Gestione.dispositivi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompitoSettimanaleGestioneDispositiviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
